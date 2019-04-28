4276b2779a7bf7a63432997aefb2fcf48d586c15
connerkward <conner.k,ward@gmail.com>
2019-04-27 18:56:52 -0700

    added internal Tile array
    for agent LOS ( line of sight )

--- src/Agent.h
+++ src/Agent.h
@@ -15,13 +15,15 @@ public:
     int health;
     int playercord [2];
     Agent();
-    Agent(int innitHealth, int spawn[2], Board &board); // b is reference to board object instatiated in main
+    Agent(int innitHealth, int spawncoords[2], Board &board); // does agent need board refernce?
     void eat(Food someFood);
     void move(int direction[2]);
     void randomMove();
     bool age();
 private:
-    Board &internalboard; // internal reference so we can check board state
+    Tile internalboard[9]; // internal "line of sight"
+    // need some way to package the internal board state
+    
 };
 
 
